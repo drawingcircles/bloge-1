4f85c03080ab6d3c647a009ed3c417263f40268e
wackmiller <jakemolinar@gmail.com>
2023-02-12 14:58:51 -0700

    allowed hosts removed from env variables

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-
 web: gunicorn bloge.wsgi --log-file -
--- bloge/settings.py
+++ bloge/settings.py
@@ -37,9 +37,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-if not DEBUG:
-    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bloge.up.railway.app']
+# if not DEBUG:
+#     ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOST')]
 
 # Application definition
 
